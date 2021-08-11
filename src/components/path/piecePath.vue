1f3f4d55db7f2c33c274ef712ba8961ac387778a
jamezzz123 <chinwokwuj@gmail.com>
2021-08-11 18:09:49 +0100

    refactor(path): :tada: make piece move along path

--- src/components/path/piecePath.vue
+++ src/components/path/piecePath.vue
@@ -15,13 +15,16 @@
       />
     </svg>
 
-    <img
-      id="item"
-      style="width: 50px"
-      src="@/assets/svg/001-gorilla.svg"
-      alt=""
-      srcset=""
-    />
+    <template v-for="item in items" :key="item.id">
+      <img
+        :id="item.id"
+        class="pieces"
+        style="width: 50px"
+        :src="item.link"
+        alt=""
+        srcset=""
+      />
+    </template>
   </div>
 </template>
 
@@ -29,22 +32,65 @@
 import { defineComponent } from "vue";
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+gsap.registerPlugin(MotionPathPlugin);
 
 export default defineComponent({
+  data() {
+    return {
+      items: [
+        {
+          id: "gorilla",
+          link: require("@/assets/svg/001-gorilla.svg"),
+        },
+        {
+          id: "owl",
+          link: require("@/assets/svg/001-owl.svg"),
+        },
+        {
+          id: "panda",
+          link: require("@/assets/svg/006-panda.svg"),
+        },
+        {
+          id: "cow",
+          link: require("@/assets/svg/005-cow.svg"),
+        },
+        {
+          id: "bull",
+          link: require("@/assets/svg/008-bull.svg"),
+        },
+        {
+          id: "rhinoceros",
+          link: require("@/assets/svg/007-rhinoceros.svg"),
+        },
+        {
+          id: "pig",
+          link: require("@/assets/svg/008-pig.svg"),
+        },
+        {
+          id: "crocodile",
+          link: require("@/assets/svg/010-crocodile.svg"),
+        },
+      ],
+    };
+  },
   setup() {
     return {};
   },
   mounted() {
-    gsap.registerPlugin(MotionPathPlugin);
-    gsap.to("#item", {
+    gsap.to(".pieces", {
       motionPath: {
         path: "#path",
         align: "#path",
         alignOrigin: [0.5, 0.5],
+
         // autoRotate: true,
       },
+      stagger: 0.3,
+      delay: 0.5,
+      // repeat: -1,
+      paused: false,
       transformOrigin: "50% 50%",
-      duration: 5,
+      duration: 10,
       ease: "power1.inOut",
     });
   },
