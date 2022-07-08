f8c3ff3603056ab2e38e95c1a72e364033678724
jamezzz123 <chinwokwuj@gmail.com>
2022-07-08 22:45:59 +0100

    test: add some e2e test

--- src/components/PropertyModal.vue
+++ src/components/PropertyModal.vue
@@ -63,6 +63,7 @@
               Cancel
             </button>
             <button
+              data-test="ok-modal-button"
               ref="ok"
               class="px-5 py-2 bg-green-600 roboto-font text-white rounded drop-shadow-md font-medium tracking-wider hover:bg-green-700 hover:drop-shadow-lg"
             >
--- tests/e2e/specs/games.js
+++ tests/e2e/specs/games.js
@@ -3,10 +3,18 @@
 describe("Go to games page", () => {
   it("Visits the app root url", () => {
     cy.visit("/Games");
-    // cy.request("POST", "/test/seed/post", {
-    //   title: "First Post",
-    //   authorId: 1,
-    //   body: "...",
-    // });
+  });
+  it("Check if decision modal opens when you land on a property", () => {
+    cy.visit("/Games");
+    cy.wait(7000);
+    cy.get('[data-test="ok-modal-button"]').and("be.visible");
+  });
+
+  it("Check if after clicking  modal closes", () => {
+    cy.get('[data-test="ok-modal-button"]').then(($btn) => {
+      $btn.click();
+    });
+
+    cy.get('[data-test="ok-modal-button"]').and("be.not.visible");
   });
 });
